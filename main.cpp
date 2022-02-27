9a2b98c2e18e5ad2df07e3aab3ecfec53bd3d54a
kpalacios <kpalaciosm02@gmail.com>
2022-02-27 15:45:25 -0600

    First Part from  time conversion ready

--- main.cpp
+++ main.cpp
@@ -32,10 +32,64 @@ void fast_triplets(int trip1[3],  int trip2[3]){
 void time_conversion_1(string hour){
     //first try of time conversion
     int length = hour.length();
-    if (hour[length-1] == 'M' && hour[length-2] == 'P')
+    if (hour[length-1] == 'M' && hour[length-2] == 'P'){
         cout << "Si hay PM" << endl;
-    else{
+        if (hour[0] == '0' && hour[1] == '1'){
+            string tmp = hour.substr(3,5);
+            cout << "13:" << tmp << endl;
+        }
+        else if (hour[0] == '0' && hour[1] == '2'){
+            string tmp = hour.substr(3,5);
+            cout << "14:" << tmp << endl;
+        }
+        else if (hour[0] == '0' && hour[1] == '3'){
+            string tmp = hour.substr(3,5);
+            cout << "15:" << tmp << endl;
+        }
+        else if (hour[0] == '0' && hour[1] == '4'){
+            string tmp = hour.substr(3,5);
+            cout << "16:" << tmp << endl;
+        }
+        else if (hour[0] == '0' && hour[1] == '5'){
+            string tmp = hour.substr(3,5);
+            cout << "17:" << tmp << endl;
+        }
+        else if (hour[0] == '0' && hour[1] == '6'){
+            string tmp = hour.substr(3,5);
+            cout << "18:" << tmp << endl;
+        }
+        else if (hour[0] == '0' && hour[1] == '7'){
+            string tmp = hour.substr(3,5);
+            cout << "19:" << tmp << endl;
+        }
+        else if (hour[0] == '0' && hour[1] == '8'){
+            string tmp = hour.substr(3,5);
+            cout << "20:" << tmp << endl;
+        }
+        else if (hour[0] == '0' && hour[1] == '9'){
+            string tmp = hour.substr(3,5);
+            cout << "21:" << tmp << endl;
+        }
+        else if (hour[0] == '1' && hour[1] == '0'){
+            string tmp = hour.substr(3,5);
+            cout << "22:" << tmp << endl;
+        }
+        else if (hour[0] == '1' && hour[1] == '1'){
+            string tmp = hour.substr(3,5);
+            cout << "23:" << tmp << endl;
+        }
+    }
+    else{ //caso AM
         cout << "No hay PM" << endl;
+        if (hour[0] == '1' && hour[1] == '2'){
+            string tmp = hour.substr(3,5);
+            cout << "00:" << tmp << endl;
+        }
+        else{
+            cout << "pase" << endl;
+            string tmp = hour.substr(0,7);
+            cout << tmp << endl;
+        }
     }
     
 }
@@ -44,6 +98,6 @@ int main(){
     int trip1[3] = {1,2,3};
     int trip2[3] = {3,2,1};
     //fast_triplets(trip1,trip2);
-    time_conversion_1("12:00:00PM");
+    time_conversion_1("01:45:30AM");
     return 0;
 }
